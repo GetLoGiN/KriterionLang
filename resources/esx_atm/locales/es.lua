9855d45757a80be75ce4dc2fe21b5874037f8ce1
Neftiaf <52339879+Neftiaf@users.noreply.github.com>
2019-06-29 23:37:29 -0500

    Update es.lua

--- resources/esx_atm/locales/es.lua
+++ resources/esx_atm/locales/es.lua
@@ -2,6 +2,6 @@ Locales['es'] = {
   ['invalid_amount'] = '~r~Cantidad no válida',
   ['deposit_money']  = 'has depositado ~g~€%s~s~',
   ['withdraw_money'] = 'has retirado ~g~€%s~s~',
-  ['press_e_atm']    = 'presiona ~INPUT_PICKUP~ para depositar o retirar ~g~cash~s~.',
+  ['press_e_atm']    = 'presiona ~INPUT_PICKUP~ para depositar o retirar ~g~dinero~s~.',
   ['atm_blip']       = 'ATM',
 }
