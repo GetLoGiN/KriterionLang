cc894c4f55486ef4336a714cb31752141547ff90
Neftiaf <52339879+Neftiaf@users.noreply.github.com>
2019-07-02 16:16:00 -0500

    Update es.lua

--- resources/esx_weaponshop/locales/es.lua
+++ resources/esx_weaponshop/locales/es.lua
@@ -1,13 +1,13 @@
 Locales ['es'] = {
-  ['buy_license'] = 'buy weapon license?',
+  ['buy_license'] = 'comprar licencia de arma?',
   ['yes'] = '%s',
   ['no'] = 'no',
-  ['weapon_bought'] = 'purchased for %s EUR',
+  ['weapon_bought'] = 'comprar por %s USD',
   ['not_enough_black'] = 'usted no tiene ~r~suficiente~s~ dinero negro',
   ['not_enough'] = 'usted no tiene ~r~suficiente~s~ dinero',
-  ['already_owned'] = 'you already own this weapon!',
+  ['already_owned'] = 'ya tienes esta arma!',
   ['shop_menu_title'] = 'tienda',
   ['shop_menu_prompt'] = 'presione ~INPUT_CONTEXT~ para acceder a la tienda.',
-  ['shop_menu_item'] = '%s EUR',
+  ['shop_menu_item'] = '%s USD',
   ['map_blip'] = 'arsenal',
 }
