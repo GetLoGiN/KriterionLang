524e141e05d3d60c1f9f4679c299f033ecd32536
Neftiaf <52339879+Neftiaf@users.noreply.github.com>
2019-07-02 16:19:55 -0500

    Update en.lua

--- resources/esx_whitelist/locales/en.lua
+++ resources/esx_whitelist/locales/en.lua
@@ -1,8 +1,8 @@
 Locales['en'] = {
-  ['whitelist_check']     = 'making sure you\'re whitelisted on this server . . .',
-  ['not_whitelisted']     = 'you are not whitelisted on this server',
-  ['steamid_error']       = 'your Steam ID was not found, is Steam running?',
-  ['whitelist_empty']     = 'the whitelist hasn\'t been loaded yet, or alternatively no one has been whitelisted!',
-  ['help_whitelist_add']  = 'add someone to the whitelist',
-  ['help_whitelist_load'] = 'reload the whitelist',
+  ['whitelist_check']     = 'asegurandose de que estas en la lista blanca de este servidor . . .',
+  ['not_whitelisted']     = 'no estas en la lista blanca de este servidor',
+  ['steamid_error']       = 'tu ID de Steam no se encontro, esta corriendo tu Steam?',
+  ['whitelist_empty']     = 'la lista blanca no se a cargado aun, o alternativamente que nadie ha sido incluido en la lista blanca!',
+  ['help_whitelist_add']  = 'agregar a alguien a la lista blanca',
+  ['help_whitelist_load'] = 'recargar la lista blanca',
 }
