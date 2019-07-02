367836468c48705cb5a8e73352fc1058fb33d6f5
Neftiaf <52339879+Neftiaf@users.noreply.github.com>
2019-07-02 17:32:28 -0500

    Update es.lua

--- resources/esx_drugs/locales/es.lua
+++ resources/esx_drugs/locales/es.lua
@@ -1,8 +1,8 @@
 Locales ['es'] = {
   -- weed
-  ['weed_pickupprompt'] = 'pulsa ~INPUT_CONTEXT~ para recolectar ~g~Marijuana húmeda~s~.',
+  ['weed_pickupprompt'] = 'presiona ~INPUT_CONTEXT~ para recolectar ~g~Marijuana húmeda~s~.',
   ['weed_inventoryfull'] = 'no puedes llevar más ~g~Marijuana húmeda~s~!',
-  ['weed_processprompt'] = 'pulsa ~INPUT_CONTEXT~ para ~g~secar la Marijuana~s~.',
+  ['weed_processprompt'] = 'presiona ~INPUT_CONTEXT~ para ~g~secar la Marijuana~s~.',
   ['weed_processingstarted'] = 'secando la ~g~Marijuana húmeda~s~...',
   ['weed_processingfull'] = 'no puedes llevar más ~g~Marijuana~s~!',
   ['weed_processingenough'] = 'necesitas ~b~3x~s~ ~g~Marijuana húmeda~s~.',
@@ -10,7 +10,7 @@ Locales ['es'] = {
   ['weed_processingtoofar'] = 'el secado se ha ~r~cancelado~s~ por que has abandonado el área.',
 
   -- drug dealer
-  ['dealer_prompt'] = 'pulsa ~INPUT_CONTEXT~ para hablar con el ~r~Camello~s~.',
+  ['dealer_prompt'] = 'presiona ~INPUT_CONTEXT~ para hablar con el ~r~Camello~s~.',
   ['dealer_title'] = 'camello',
   ['dealer_item'] = '$%s',
   ['dealer_notenough'] = 'esa mierda no es cantidad suficiente para vender!',
